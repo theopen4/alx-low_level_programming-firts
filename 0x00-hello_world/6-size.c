172ffb1df4dc892b6d101ab4818bab74a97445ab
kielze86@gmail.com <kielz>
2022-07-03 16:36:20 -0700

    tasktrue

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,14 +10,13 @@ int i;
 long int l;
 long long int ll;
 float d;
-char c; 
-
+char c;
 
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
-
 printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-printf("Size of a float: %lu byte(s)\n", sizeof(d));	
+printf("Size of a float: %lu byte(s)\n", sizeof(d));
+
 return (0);
 }
