23c3656f3a1d306bfc8f456de7a072d37aeb7556
kielze86@gmail.com <kielz>
2022-07-03 12:54:55 -0700

    tasktrue

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,18 @@
 #!/bin/bash
-export CFILE=3-main.c ; ./3-name
+#include <stdio.h>
+
+/**
+
+ * main - Entry point
+ *
+ *  * Return: Always 0 (sucess)
+ *
+ *   */
+
+ int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
