915a03924cf6e81d8d8c2fac2a0f77ba5db4e2f3
kielze86@gmail.com <kielz>
2022-07-04 14:50:11 -0700

    task_if

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@void - no parameter
+ *Return: 0
+      */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{					{
+printf("%d", i);									}
+printf("\n");
+}
+
+return (0);
+}
+
