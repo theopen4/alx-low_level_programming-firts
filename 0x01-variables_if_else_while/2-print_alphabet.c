34e59390cdf1fbd0c6fcdd07fbed71b4920b5de5
kielze86@gmail.com <kielz>
2022-07-04 14:29:15 -0700

    task_if

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - prints lower alphabets from a - z
+ *@void: no parameters
+ *Return: 0
+       */
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);									}
+putchar('\n');
+
+
+return (0);
+}
