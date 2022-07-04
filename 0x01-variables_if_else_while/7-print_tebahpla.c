cbe4fd6c5f84fa74c3e9e42121599c1d13aecf69
kielze86@gmail.com <kielz>
2022-07-04 15:13:44 -0700

    task_if

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0;
+      */
+int main(void)
+{
+char ch = 'z';
+
+while (ch >= 'a')
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+
+return (0);
+}
