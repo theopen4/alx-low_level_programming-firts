6f2c0f25dfebd1d42344fbfb1bff877f7f674575
kielze86@gmail.com <kielz>
2022-07-04 18:26:39 -0700

    truetask

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *   main - prints to the standard error without using
+ * @void: no parameter:
+      * Return: 1
+   */
+int main(void)
+{
+write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+return (1);
+}
+
