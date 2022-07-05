58c563922fc07f09c6726d01e8014ace1c0a787a
kielze86@gmail.com <kielz>
2022-07-05 16:41:38 -0700

    taskFunction

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *   main - entry point
+ *    @void: no parameter
+ *      Return: 0
+       */
+int main(void)
+{
+
+printf("_putchar\n");
+printf("\n");
+return (0);
+
+}
