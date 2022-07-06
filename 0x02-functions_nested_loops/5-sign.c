2f105e6de976be98e80e39c10293a3aed26c8847
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-06 12:36:53 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,26 @@
-sdf
+#include "main.h"
+/**
+ * print_sign - prints signs of a number
+ * @n: takes integer paramer n
+ * Return: 1 if the number is positive
+ * 0 if the number is zero
+ * -1 if the number is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
