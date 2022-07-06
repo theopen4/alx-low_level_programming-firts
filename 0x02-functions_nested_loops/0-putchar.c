d92ad36835253aedeb4207efba54671fe9117d9b
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-06 12:05:15 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
-jZ
+#include "main.h"
+/**
+ * main - prints _putchar
+ * @void: doesn't take any parameter
+ *
+ * Return: 0 (always success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
