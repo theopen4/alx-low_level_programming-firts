285a0317f9477ac90e77236ca79acf1e9a657172
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-06 15:04:14 +0200

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,18 @@
-dfgfd
+#include <stdio.h>
+/**
+ * main - prints the sum of multiples of 3 and 5
+ * Return: 0 which means successful.
+ */
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
