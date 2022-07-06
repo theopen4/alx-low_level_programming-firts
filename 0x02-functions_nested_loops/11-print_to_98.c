44af7849e739ceea12e3d19fa4a8a18122a335ac
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-06 14:23:34 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,21 @@
-dfdsf
+#include <stdio.h>
+/**
+ * print_to_98 - prints number form n to 98
+ *@n: is the start of the list
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
