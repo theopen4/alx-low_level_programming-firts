3ab883228df34f6546f5d9968994c9e750bb7d0d
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-06 15:16:27 +0200

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,23 @@
-gfdg
+#include <stdio.h>
+/**
+ * main - sums up the even fibonacci numbers with bound
+ * Return: 0 (always sucess)
+ */
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, next_fib;
+	float total;
+
+	while (1)
+	{
+		next_fib = fib1 + fib2;
+		if (next_fib > 4000000)
+			break;
+		if (next_fib % 2 == 0)
+			total += next_fib;
+		fib1 = fib2;
+		fib2 = next_fib;
+	}
+	printf("%.0f\n", total);
+	return (0);
+}
