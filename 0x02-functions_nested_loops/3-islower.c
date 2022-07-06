1c540f2e0fdc734bc614cfa17f6e76c2363f79d3
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-06 12:26:20 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,13 @@
-iiZZ
+#include "main.h"
+/**
+ * _islower - checkes lowercase
+ * @c: takes integral parameter c
+ * Return: 1 if lowercase or 0  if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
