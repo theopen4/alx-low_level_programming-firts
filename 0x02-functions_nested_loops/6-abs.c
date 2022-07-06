9459f21ab755ad5df700e462d6c45a0772cb34ad
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-06 13:47:54 +0200

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,13 @@
-vZZfd
+#include "main.c"
+/**
+ * _abs - computes the absolute value of a number
+ * @c: takes c as a parameter
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
