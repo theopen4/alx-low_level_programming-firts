0924c0cc7d03b530660c2f3a2517a602f1dff8e0
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-06 14:20:44 +0200

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,14 @@
-oio
+#include "main.h"
+/**
+ * add - adds two number
+ * @x: operand to be added
+ * @y: operand to be added
+ * Return: integer value
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
