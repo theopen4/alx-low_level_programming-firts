342c8aa6aa1c2267786adb52b40337e697720c90
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-08 00:34:23 +0200

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,14 @@
-ggd
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: parameter a
+ * @b: parameter b
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
