6ce65c2cabe9212534d8f90e832107dee16eedc5
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-07 22:17:33 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,15 @@
-#include 0;
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: zero
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
 positive_or_negative(i);
 
 return (0);
