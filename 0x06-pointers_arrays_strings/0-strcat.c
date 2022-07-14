101c003dce464e3a4234da622b778bdeb46aedf5
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-14 21:33:29 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,12 @@
-gfdgfdgfdk
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{ 
+ return strcat(*dest, *src);  
+}
