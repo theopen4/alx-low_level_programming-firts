6cab1458348ad69d3452a7db1858fff9caa8d121
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-20 23:23:22 +0200

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -17,12 +17,6 @@ int is_palindrome(char *s)
 	return (check_palindrome(s, len, index));
 }
 
-/**
- * find_strlen - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
- */
 int find_strlen(char *s)
 {
 	int len = 0;
@@ -36,15 +30,6 @@ int find_strlen(char *s)
 	return (len);
 }
 
-/**
- * check_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
- * @len: The length of s.
- * @index: The index of the string to be checked.
- *
- * Return: If the string is a palindrome - 1.
- *         If the string is not a palindrome - 0.
- */
 int check_palindrome(char *s, int len, int index)
 {
 	if (s[index] == s[len / 2])
