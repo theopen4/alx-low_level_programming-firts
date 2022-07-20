041325c7c52609e19debc392920126ad256b6690
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-20 23:46:34 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,43 @@
 #include "main.h"
-/**
-* is_prime_number - returns true if the number is prime
-*@n: the number to check
-*
-*Return: true if the number is prime
-*/
-int is_prime_number(int n)
-{
-	int start = n / 2;
 
-	if (n <= 1)
-		return (0);
-	return (is_prime(n, start));
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
+/
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: If the number is divisible - 0.
+ * If the number is not divisible - 1.
+ */
+int is_divisible(int num, int div)
+{
+  if (num % div == 0)
+    return (0);
+
+  if (div == num / 2)
+    return (1);
+
+  return (is_divisible(num, div + 1));
 }
 
-/**
-* is_prime - returns the 1 if n is prime
-* @n: number to be checked
-* @start: number to start checking from
-*
-* Return: 1 if n is prime, 0 otherwise
-*/
-
-int is_prime(int n, int start)
+/
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number is to be checked.
+ *
+ * Return: If the integer is not prime - 0.
+ * If the number is prime - 1.
+ */
+int is_prime_number(int n)
 {
-	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
-		return (0);
-	return (is_prime(n, start - 1));
+  int div = 2;
+
+  if (n <= 1)
+    return (0);
+
+  if (n >= 2 && n <= 3)
+    return (1);
+
+  return (is_divisible(n, div));
 }
