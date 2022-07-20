d5c041337b7f75818ce2dde16916752a95b86e1d
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-21 00:40:14 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,4 @@
+The4 Theopen4, [20/07/2022 23:47]
 #include "main.h"
 
 int find_sqrt(int num, int root);
@@ -13,13 +14,13 @@ int _sqrt_recursion(int n);
  */
 int find_sqrt(int num, int root)
 {
-  if ((root * root) == num)
-    return (root);
+ if ((root * root) == num)
+  return (root);
 
-  if (root == num / 2)
-    return (-1);
+ if (root == num / 2)
+  return (-1);
 
-  return (find_sqrt(num, root + 1));
+ return (find_sqrt(num, root + 1));
 }
 
 /**
@@ -31,12 +32,12 @@ int find_sqrt(int num, int root)
  */
 int _sqrt_recursion(int n)
 {
-  int root = 0;
+ int root = 0;
 
-  if (n < 0)
-    return (-1);
-  if (n == 1)
-    return (1);
+ if (n < 0)
+  return (-1);
+ if (n == 1)
+  return (1);
 
-  return (find_sqrt(n, root));
+ return (find_sqrt(n, root));
 }
