57af80f0e1399b548929a49482f96e538a70ed44
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-23 00:06:55 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,14 @@
-dfgfdgjdfk
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - the main function to print the programs name followed by a new line
+ * @argc: a number of arguments passed to main function
+ * @argv: an array containing a string of arguments
+ * Return: 0 (always success)
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
