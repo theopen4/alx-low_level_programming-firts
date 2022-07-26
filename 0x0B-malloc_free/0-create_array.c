f3b94de7430096693eebdbeed3947f96701d6641
theopen4 <66733800+theopen4@users.noreply.github.com>
2022-07-26 23:36:25 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,1 +1,27 @@
-gfdgdf
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Function that creates an array of chars with malloc
+ * @size: Size of array
+ * @c: character of buffer
+ * Return: An array of char
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
